0acdf2667436602fc9af8cd9d1b687a292c57fee
Hernandez <dhernandez253@cps.k12.il.us>
2023-10-04 08:54:24 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# data abstruction - when you need to simtify data 
+# from something more complex
+
+#an example of data absraction is when I use a list to simplify data
+#instead of having to write out the same code over and over again
+#data abstraction is when you simplify data
+# if you have a list of names, you can simplify it by using a list
+
+
+
